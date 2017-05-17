b4a039686eb945d9478cee92b8605ac0b4e4b1d5
David Merrick <david.merrick@jivesoftware.com>
2017-05-16 21:35:06 -0700

    Added code

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const axios = require('axios');
+
+exports.handler = (event, context, callback) => {
+    console.log('Received event:', event.clickType);
+
+    var auth = {
+        username: process.env.AUTH_USERNAME,
+        password: process.env.AUTH_PASSWORD
+    };
+
+    var options = {
+        auth: auth
+    };
+
+    console.log("Playing via Sonos API server...");
+    axios.get(`${process.env.SONOS_API_SERVER}/playpause`, options)
+        .then(response => {
+            console.log("SUCCESS.");
+            return callback();
+        })
+        .catch(error => {
+            console.error("ERROR");
+            return callback();
+        });
+};
